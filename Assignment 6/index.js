88a26d0e4d430f79807e9ce129421a603cd79a3e
manavdas1999 <dascorp009@gmail.com>
2024-03-10 12:32:59 +0530

    Assignment 6 : upto Q8

--- Assignment 6/index.js
+++ Assignment 6/index.js
@@ -103,3 +103,151 @@ function toStringArray(arr){
 // Q4([ 5, 1, 2, 3 ], [ 2, 5, 1, 3 ])
 
 
+/*
+5.
+          1 
+        2 1 2
+      3 2 1 2 3
+    4 3 2 1 2 3 4
+  5 4 3 2 1 2 3 4 5
+6 5 4 3 2 1 2 3 4 5 6
+*/
+
+function Q5(num){
+    for(let i=1; i<=num; i++){
+        // for spaces
+        for(let s=i; s<=num-1; s++){
+            process.stdout.write("  ");
+        }
+        // for decreasing value
+        for(let j=i; j>=1; j--){
+            process.stdout.write(j +" ");
+        }
+        // for increasing value
+        for(let k=2; k<=i; k++){
+            process.stdout.write(k + " ");
+        }
+        // newline
+        console.log()
+    }
+}
+// Q5(6)
+
+
+
+/*
+6. Pascal's Triangle
+// TODO
+*/
+
+
+
+/*
+7. 
+
+*/
+
+
+/*
+8) Write a program to perform the following operations on the array of numbers
+I. Increment every array element by 2 and print
+II. Decrement every array element by 2 and print
+III. Square every array element
+IV. Reset every array element to 1
+V. Print Prime numbers in array
+VI. Print Check Even/Odd numbers in array
+VII. Check Palindrome numbers in array
+VIII. Compute Factorial of numbers in array
+IX. Remove first element from array
+X. Insert two elements at second position
+*/
+
+
+// I
+function Q81(arr){
+    for(let i=0; i<arr.length; i++){
+        arr[i] = arr[i] + 2
+    }
+
+    console.log(arr)
+}
+
+function Q82(arr){
+    for(let i=0; i<arr.length; i++){
+        arr[i] = arr[i] - 2
+    }
+
+    console.log(arr)
+}
+
+function Q83(arr){
+    for(let i=0; i<arr.length; i++){
+        arr[i] = arr[i] * arr[i]
+    }
+}
+
+function Q84(arr){
+    for(let i=0; i<arr.length; i++){
+        arr[i] = 1
+    }
+}
+
+function Q85(arr){
+    for(let i=0; i<arr.length; i++){
+        let isPrime = true
+        for(let j=2; j<=arr[i]/2; j++){
+            if(arr[i] % j == 0) {
+                isPrime = false
+                break
+            }
+        }
+        if(isPrime) console.log(arr[i])      
+    }
+}
+
+function Q86(arr){
+    for(let i of arr){
+        console.log(`${i} - ${i%2==0 ? 'even' : 'odd'}`)
+    }
+}
+
+function Q87(arr){
+    for(let i of arr){
+        let isPalindrome = String(i) == String(i).split("").reverse().join("")
+        console.log(`${i} - ${isPalindrome ? 'palindrome' : 'not palindrome'}`)
+    }
+}
+
+function Q88(arr){
+    let factorials = []
+    for(let i of arr){
+        let fact = 1
+        for(let f = 2; f<=i; f++) fact *= f
+        factorials.push(fact)
+    }
+    console.log(factorials)
+}
+
+function Q89(arr){
+    arr.shift()
+}
+
+function Q810(arr, val1, val2){
+    arr = arr.splice(1, 0, val1, val2)
+}
+
+let myArr = [11, 5, 12, 4]
+// Q81(myArr)
+// Q82(myArr)
+// Q83(myArr)
+// console.log(myArr)
+// Q84(myArr)
+// console.log(myArr)
+// Q85(myArr)
+// Q86(myArr)
+// Q87(myArr)
+// Q88(myArr)
+// Q89(myArr)
+// console.log(myArr)
+// Q810(myArr, 30, 31)
+// console.log(myArr)
